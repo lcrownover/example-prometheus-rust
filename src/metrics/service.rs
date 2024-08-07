039fab489b30c77fc4eabcd659eb87c2ef3d7f71
Lucas Crownover <lcrownover127@gmail.com>
2024-08-06 22:46:01 -0700

    readme

--- src/metrics/service.rs
+++ src/metrics/service.rs
@@ -2,20 +2,6 @@ use prometheus_client::metrics::family::Family;
 use prometheus_client::metrics::gauge::Gauge;
 use prometheus_client_derive_encode::{EncodeLabelSet, EncodeLabelValue};
 
-//
-// In prometheus, you have 4 important pieces of information that make up a metric:
-// 1. metric name
-// 2. metric help text
-// 3. metric label(s)
-// 4. metric value
-//
-// The easiest way I can think of it is that the metric name and value are the 
-// descriptive name and value of the metric you're trying to measure, whereas the 
-// labels are like tags where you can filter based on them. Many systems will probably
-// report the same metric name with different values, but the labels for each system
-// should be different to allow separation.
-//
-
 #[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelValue)]
 pub enum ServiceStateLabel {
     Stopped,
